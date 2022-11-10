7cb4a24099c32e7098a36f2e800333c3718b16af
shreeja-shreeja <115733593+shreeja-shreeja@users.noreply.github.com>
2022-11-10 22:35:52 +0100

    Update triangle.py

--- triangle.py
+++ triangle.py
@@ -1,10 +1,31 @@
-def equilateral(sides):
-    pass
+def check_valid(sides):
+    for side in sides:
+        if side <=0:
+            return False
+    return True
 
+def equilateral(sides):
+    if not check_valid(sides):
+        return False
+    if (sides[0]==sides[1]) and (sides[0]==sides[2]):
+        return True
+    else:
+        return False
 
 def isosceles(sides):
-    pass
-
+    if not check_valid(sides):
+        return False
+    if not equilateral(sides):
+        if (sides[0]==sides[1]) or (sides[1]==sides[2]) or (sides[0]==sides[2]):
+            return True
+        else:
+            return False
+    return False
 
 def scalene(sides):
-    pass
+    if not check_valid(sides):
+        return False
+    if (sides[0]!=sides[1]) and (sides[0]!=sides[2]) and (sides[1]!=sides[2]):
+        return True
+    return False
+    
