374aa9e8dc8a539c7721d483287eb0f848db5fe4
shreeja-shreeja <115733593+shreeja-shreeja@users.noreply.github.com>
2022-11-10 22:38:26 +0100

    Update triangle.py

--- triangle.py
+++ triangle.py
@@ -15,12 +15,10 @@ def equilateral(sides):
 def isosceles(sides):
     if not check_valid(sides):
         return False
-    if not equilateral(sides):
-        if (sides[0]==sides[1]) or (sides[1]==sides[2]) or (sides[0]==sides[2]):
-            return True
-        else:
-            return False
-    return False
+    if (sides[0]==sides[1]) or (sides[1]==sides[2]) or (sides[0]==sides[2]):
+        return True
+    else:
+        return False
 
 def scalene(sides):
     if not check_valid(sides):
